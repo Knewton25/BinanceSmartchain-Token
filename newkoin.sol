992c687b9df5473a0dd34320ae841fa3887222f6
Newton <125439688+Knewton25@users.noreply.github.com>
2024-12-30 23:31:30 -0800

    Update newkoin.sol

--- newkoin.sol
+++ newkoin.sol
@@ -790,12 +790,4 @@ contract NEWKOIN is Context, IERC20, Ownable {
             feeAmount = amount.mul(_totalTaxIfSelling).div(100);
         }
         
-        if(feeAmount > 0) {
-            _balances[address(this)] = _balances[address(this)].add(feeAmount);
-            emit Transfer(sender, address(this), feeAmount);
-        }
-
-        return amount.sub(feeAmount);
-    }
-    
-}
+      
